752866c79b792fc2ccb2ecaad1d09c4fe8ee9660
Danieldaviddf <danieldaviddf@hotmail.com>
2026-02-27 12:38:17 +0100

    carta final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+    
+
+  function generador(){
+
+    let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+      let dibujo = ["♦", "♥", "♠", "♣"];
+
+      let numeroRd = numeros[Math.floor(Math.random()*numeros.length)];
+      let dibujoRd = dibujo[Math.floor(Math.random()*dibujo.length)];
+
+      let head = document.querySelector("#head");
+        head.innerHTML = dibujoRd;
+
+      let center = document.querySelector("#center");
+        center.innerHTML = numeroRd;
+
+      let bottom = document.querySelector("#bottom");
+        bottom.innerHTML = dibujoRd;
+
+      if(dibujoRd === "♥" || dibujoRd === "♦") {
+        head.style.color = "red";
+        bottom.style.color = "red";
+      } else {
+        head.style.color = "black";
+        bottom.style.color = "black";
+      }
+
+  } 
+  generador();
+  document.getElementById("botoncito").addEventListener("click", generador);
 };
+
+
